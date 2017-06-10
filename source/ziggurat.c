64156c632cad930077ee338ed8eda86b54ba67b5
Attila Kinali <attila@kinali.ch>
2017-06-10 16:21:11 +0200

    add ziggurat code from Lee Daniel Crocker
    Downloaded from https://github.com/lcrocker/ojrandlib
    
    Signed-off-by: Attila Kinali <attila@kinali.ch>

---
+++ source/ziggurat.c
@@ -0,0 +1,113 @@
+/* OneJoker RNG library <http://lcrocker.github.io/onejoker/randlib>
+ *
+ * To the extent possibile under law, Lee Daniel Crocker has waived all
+ * copyright and related or neighboring rights to this work.
+ * <http://creativecommons.org/publicdomain/zero/1.0/>
+ *
+ * This code is based on George Marsaglia's Ziggurat algorithm, with
+ * various improvements from Jurgen A. Doornik and me. I believe it is
+ * sufficiently dissimilar to any pre-existing implementation that there
+ * should be no copyright problems, so my CC0 dedication applies.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+
+#include "ojrandlib.h"
+
+// On some (32-bit) machines, comparison of doubles might actually be
+// faster than 64-bit integer compares, so comment out this define
+#define INTEGER_COMPARE 1
+#include "zigtables.h"
+
+// The sizes of 128 and 256 from GM's paper are good, but I recalculated
+// the constants here and built fixed tables based on them.
+
+#define ZNR128 3.4426198558966521214
+#define ZNV128 0.0099125630353364610791
+#define ZER256 7.6971174701310497140
+#define ZEV256 0.0039496598225815572200
+
+double ojr_next_exponential(ojr_generator *g) {
+    uint64_t r;
+    int i;
+    double x, u0, f0, f1;
+
+    while (1) {
+        r = OJR_NEXT64(g);
+        i = r & 0xFF;
+        r = (r >> 8) & 0xFFFFFFFFFFFFFULL;
+
+#ifdef INTEGER_COMPARE
+        if (r < zeri[i]) {
+            r |= 0x3FF0000000000000ULL;
+            u0 = *(double *)(&r) - 1.0;
+            return u0 * zex[i];
+        }
+#else
+        r |= 0x3FF0000000000000ULL;
+        u0 = *(double *)(&r) - 1.0;
+
+        if (u0 < zer[i]) return u0 * zex[i];
+#endif
+        if (0 == i) return ZER256 - log(ojr_next_double(g));
+
+#ifdef INTEGER_COMPARE
+        r |= 0x3FF0000000000000ULL;
+        u0 = *(double *)(&r) - 1.0;
+#endif
+        x = u0 * zex[i];
+        f0 = exp(x - zex[i]);
+        f1 = exp(x - zex[i+1]);
+        if (f1 + ojr_next_double(g) * (f0 - f1) < 1.0) return x;
+    }
+}
+
+double ojr_next_normal(ojr_generator *g) {
+    uint64_t r;
+    int i, sign;
+    double x, y, a, u0, f0, f1;
+
+    while (1) {
+        do {
+            r = OJR_NEXT64(g);
+            sign = (int)r & 1;
+            i = (r >> 1) & 0x7F;
+            r >>= 12;
+        } while (sign && 0LL == r);
+
+#ifdef INTEGER_COMPARE
+        if (r < znri[i]) {
+            r |= 0x3FF0000000000000ULL;
+            a = *(double *)(&r) - 1.0;
+            return znx[i] * (sign ? -a : a);
+        }
+#else
+        r |= 0x3FF0000000000000ULL;
+        a = *(double *)(&r) - 1.0;
+        u0 = sign ? -a : a;
+
+        if (a < znr[i]) return u0 * znx[i];
+#endif
+        if (0 == i) {
+            do {
+                x = log(ojr_next_double(g)) / ZNR128;
+                y = log(ojr_next_double(g));
+            } while (-2.0 * y < x * x);
+            return sign ? x - ZNR128 : ZNR128 - x;
+        }
+#ifdef INTEGER_COMPARE
+        r |= 0x3FF0000000000000ULL;
+        a = *(double *)(&r) - 1.0;
+        x = znx[i] * (sign ? -a : a);
+#else
+        x = u0 * znx[i];
+#endif
+        f0 = exp(-0.5 * (znx[i] * znx[i] - x * x));
+        f1 = exp(-0.5 * (znx[i+1] * znx[i+1] - x * x));
+        if (f1 + ojr_next_double(g) * (f0 - f1) < 1.0) return x;
+    }
+}
