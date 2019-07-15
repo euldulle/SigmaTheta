4e7d39a45480236bff0980bada6954cf633d59cf
francois.meyer <francois.meyer@univ-fcomte.fr>
2019-07-15 10:39:41 +0000

    load_ykt can now handle stdin input

--- source/stio_sbr.c
+++ source/stio_sbr.c
@@ -368,7 +368,12 @@ int load_ykt(char *source)
     dtmx=DATAMAX;
     T=(double *)malloc(dtmx*sizeof(double)); 
     Y=(double *)malloc(dtmx*sizeof(double)); 
-    ofd=fopen(source, "r");
+
+    if (strlen(source)==0)
+        ofd=stdin;
+    else
+        ofd=fopen(source, "r");
+
     if (ofd==NULL)
         return(-1);
     else
@@ -401,7 +406,8 @@ int load_ykt(char *source)
 	            }
 	        }
             while(fscanf(ofd,"%lf %lf",&T[i],&Y[i])==2);
-            fclose(ofd);
+            if (ofd!=stdin)
+                fclose(ofd);
             N=i;
 	    }
         }
